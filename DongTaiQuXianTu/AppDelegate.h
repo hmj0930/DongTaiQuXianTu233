9cced66548a916f3beafe7396a0254bb5bfc3ee6
HanMJ <915660592@qq.com>
2017-07-20 10:43:24 +0800

    Initial Commit

---
+++ DongTaiQuXianTu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DongTaiQuXianTu
+//
+//  Created by MJ on 2017/7/20.
+//  Copyright © 2017年 韩明静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
